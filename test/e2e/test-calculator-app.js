3afa32c0cf622e2f7986012df71b6ac19a4afc1a
Gil Tayar <gil@tayar.org>
2017-02-19 10:32:04 +0200

    now serving calculator app

--- test/e2e/test-calculator-app.js
+++ test/e2e/test-calculator-app.js
@@ -1,22 +1,31 @@
 const {describe, it, before, after} = require('mocha')
 const {expect} = require('chai')
+const path = require('path')
+const express = require('express')
 const {prepareBrowser, cleanupBrowser} = require('../utils/browser-automation')
 
 describe('calculator app', function () {
   let browser
+  let server
 
   this.timeout(30000)
+
+  before((done) => {
+    const app = express()
+    app.use('/', express.static(path.resolve(__dirname, '../../dist')))
+    server = app.listen(8080, done)
+  })
+  after(() => {
+    server.close()
+  })
 
   before(async () => {
     browser = await prepareBrowser(this)
   })
-
   after(() => cleanupBrowser(browser))
 
-
   it('should work', async function () {
-    console.log(browser)
-    await browser.get('http://www.google.com')
+    await browser.get('http://localhost:8080')
 
     expect(await browser.getTitle()).to.equal('Calculator')
   })
